f90aa0ce79dd95bda78a4a3fc43a58b5eb101ff5
ares202316 <142474849+ares202316@users.noreply.github.com>
2026-03-03 21:15:29 -0600

    Update whatsappControllers.js

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,9 +1,14 @@
 const VerifyToken = (req, res) => {
-  const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN; // el mismo que pusiste en Meta
+  const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN;
 
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
+
+  console.log("mode:", mode);
+  console.log("token:", token);
+  console.log("challenge:", challenge);
+  console.log("env:", VERIFY_TOKEN);
 
   if (mode === "subscribe" && token === VERIFY_TOKEN) {
     return res.status(200).send(challenge);
@@ -11,9 +16,3 @@ const VerifyToken = (req, res) => {
 
   return res.sendStatus(403);
 };
-
-const ReceiveMessage = (req, res) => {
-  res.sendStatus(200);
-};
-
-module.exports = { VerifyToken, ReceiveMessage };
