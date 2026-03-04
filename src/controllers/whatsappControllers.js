bd3cbb143786c5feee8661f258dd61d3670d9b84
ares202316 <142474849+ares202316@users.noreply.github.com>
2026-03-03 20:56:05 -0600

    prueba

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,5 +1,21 @@
 const VerifyToken = (req, res) => {
-    res.send("Hola verificandoToken")
+   
+    try{
+        var accessToken = "NCUINC2373F8E8CB83C632EDD";
+        var token = req.query("hub.verify?token");
+        var challenge = req.body["hub.challenge"];
+
+        if(challenge != null && token != null && token == accessToken){
+            res.send(challenge);
+        }else{
+            res.status(400).send();
+        }
+
+    }catch(e){
+        res.status(400).send();
+    }
+
+
 }
 
 const ReceiveMessage = (req, res) => {
